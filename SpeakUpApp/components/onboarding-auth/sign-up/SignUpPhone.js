65acf463f7fd8293e6f58eea8a06554a2eef4fa7
Cameron <cameronrasmo@gmail.com>
2020-06-23 17:41:37 -0600

    Need to figure out why keyboard hides on phone input when setting state

--- SpeakUpApp/components/onboarding-auth/sign-up/SignUpPhone.js
+++ SpeakUpApp/components/onboarding-auth/sign-up/SignUpPhone.js
@@ -1,27 +1,34 @@
-import React, { useState, useRef } from 'react';
-import { View, Text, TouchableOpacity, ScrollView, TextInput } from 'react-native';
+import React, {useState, useRef} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  TextInput,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
 import PhoneInput from '@nandorojo/react-native-phone-input';
 // import ModalPickerImage from './ModalPickerImage';
 
 const SignUpPhone = ({navigation}) => {
-    const [ phoneNumber, setPhoneNumber ] = useState('+1 MOBILE NUMBER');
-    const onChangePhone = phone => setPhone(phone);
+  const [phoneNumber, setPhoneNumber] = useState('+1 MOBILE NUMBER');
+  const [keyboardToggle, setKeyboardToggle] = useState(false);
 
-    const [phoneValue, setPhoneValue] = useState(null);
-    // const [isValidNumber, setIsValidNumber] = useState(false)
-    // const phoneRef = useRef(undefined);
+  // const [isValidNumber, setIsValidNumber] = useState(false)
+  // const phoneRef = useRef(undefined);
 
-    const Main = styled.View`
+  const Main = styled.View`
     flex: 1;
     align-items: center;
     justify-content: flex-start;
     width: 100%;
     background-color: #242633;
     opacity: 1;
-    `;
-    const Header = styled.Text`
+  `;
+  const Header = styled.Text`
     width: 100%;
     font-family: Avenir;
     font-style: normal;
@@ -31,8 +38,8 @@ const SignUpPhone = ({navigation}) => {
     letter-spacing: 0.105751px;
     color: #ffffff;
     margin: 116px auto 25px;
-    `;
-    const TextHeader = styled.Text`
+  `;
+  const TextHeader = styled.Text`
     font-family: Avenir;
     font-weight: 800;
     font-size: 28px;
@@ -40,10 +47,9 @@ const SignUpPhone = ({navigation}) => {
     color: #ffffff;
     letter-spacing: 0.105751px;
     margin-top: 10px;
-    `;
-    const PhoneInputEntry = styled.TextInput`
-    background-color: rgba(255, 255, 255, 0.8);
-    border-radius: 5px;
+  `;
+  const PhoneInputEntry = styled.TextInput`
+    background: #fff;
     text-transform: uppercase;
     font-family: Avenir;
     font-style: normal;
@@ -56,8 +62,9 @@ const SignUpPhone = ({navigation}) => {
     height: 56px;
     padding: 5px;
     margin: 1px auto;
-    `;
-    const TextDisclaimer = styled.Text`
+    border-radius: 5px;
+  `;
+  const TextDisclaimer = styled.Text`
     font-family: Avenir;
     font-style: normal;
     font-weight: 500;
@@ -68,31 +75,32 @@ const SignUpPhone = ({navigation}) => {
     width: 100%;
     height: 40px;
     opacity: 0.5;
-    margin: 10px auto;
-    flex: 1;
-    `;
-// DELETE PHONE INPUT VIEW STYLING IF REMOVING PACKAGE
-    const PhoneInputView = styled.View`
+    margin: 0px auto;
+    flex: 0.3;
+    /* border: 2px solid red; */
+  `;
+  // DELETE PHONE INPUT VIEW STYLING IF REMOVING PACKAGE
+  const PhoneInputView = styled.View`
     background-color: #dedede;
     padding: 15px 10px;
     border-radius: 5px;
-    `;
-    const ButtonView = styled.View`
-    justify-content: flex-end;
+  `;
+  const ButtonView = styled.View`
+    justify-content: ${keyboardToggle ? 'flex-start' : 'flex-end'};
     width: 100%;
     flex: 1;
-    `;
-    const SignupButton = styled.TouchableOpacity`
+  `;
+  const SignupButton = styled.TouchableOpacity`
     align-items: center;
     justify-content: flex-end;
     width: 100%;
     padding-top: 20px;
     padding-bottom: 20px;
-    margin: 15px auto 0;
+    margin: 5px auto 0;
     border: 2px solid #2cc3f8;
     border-radius: 5px;
-    `;
-    const SignupText = styled.Text`
+  `;
+  const SignupText = styled.Text`
     color: #2cc3f8;
     text-transform: uppercase;
     font-family: Avenir;
@@ -101,9 +109,9 @@ const SignUpPhone = ({navigation}) => {
     font-size: 14px;
     line-height: 19px;
     letter-spacing: 0.94px;
-    `;
-// DELETE VALID NUMBER STYLING IF REMOVING PACKAGE
-    const ValidNumber = styled.Text`
+  `;
+  // DELETE VALID NUMBER STYLING IF REMOVING PACKAGE
+  const ValidNumber = styled.Text`
     color: #666;
     text-transform: uppercase;
     font-family: Avenir;
@@ -113,27 +121,39 @@ const SignUpPhone = ({navigation}) => {
     line-height: 19px;
     letter-spacing: 0.94px;
     padding-top: 2px;
-    `;
-    
-    return (
-        <Main>
-            <LinearGradient
-            colors={['#24263399', '#24263399', '#242633', '#242633', '#242633']}
-            style={{
-            width: '100%',
-            flex: 1,
-            justifyContent: 'flex-start',
-            paddingLeft: 30,
-            paddingRight: 30,
-            paddingBottom: 65,
-            }}>
-                <Header>
-                    <TextHeader>Enter your phone number</TextHeader>
-                    <TextHeader  style={{ fontSize: 20, color: '#2cc3f8'}}>{"\n"}or NEXT to use email</TextHeader>
-                </Header>
-                {/* INPUT BELOW WILL NEED TO TIE TO PHONE BLOCK CAMERON IS BUILDING */}
-                <PhoneInputEntry  value={phoneNumber} onChangeText={onChangePhone} />
-                {/* <PhoneInputView>                     
+  `;
+
+  function onChangePhoneNumber(val) {
+    console.log(val);
+  }
+
+  return (
+    <Main>
+      <LinearGradient
+        colors={['#24263399', '#24263399', '#242633', '#242633', '#242633']}
+        style={{
+          width: '100%',
+          flex: 1,
+          justifyContent: 'flex-start',
+          paddingLeft: 30,
+          paddingRight: 30,
+          paddingBottom: 65,
+        }}>
+        <Header>
+          <TextHeader>Enter your phone number</TextHeader>
+          <TextHeader style={{fontSize: 20, color: '#2cc3f8'}}>
+            {'\n'}or NEXT to use email
+          </TextHeader>
+        </Header>
+        {/* INPUT BELOW WILL NEED TO TIE TO PHONE BLOCK CAMERON IS BUILDING */}
+        {/* <PhoneInputView> */}
+        <PhoneInputEntry
+          value={phoneNumber}
+          onChangeText={text => onChangePhoneNumber(text)}
+          onFocus={() => setKeyboardToggle(prev => true)}
+        />
+        {/* </PhoneInputView> */}
+        {/* <PhoneInputView>                     
                         <PhoneInput
                             ref={phoneRef}
                             onChangePhoneNumber={value => {
@@ -143,22 +163,23 @@ const SignUpPhone = ({navigation}) => {
                         />
                         <ValidNumber>ENTER HERE: Is Valid Number: {isValidNumber ? 'Yes' : 'No'}</ValidNumber>
                 </PhoneInputView> */}
-                    <TextDisclaimer >{"\n"}By entering and tapping Next, you agree to the &nbsp;
-                        <Text style={{textDecorationLine: 'underline'}}>Terms,</Text> &nbsp;
-                        <Text style={{textDecorationLine: 'underline'}}>E-Sign Consent</Text>&nbsp;
-                        & &nbsp;
-                        <Text style={{textDecorationLine: 'underline'}}>Privacy Policy</Text>.
-                    </TextDisclaimer>
-                    <TextHeader  style={{ fontSize: 20, color: 'red', marginBottom: 0}}>{"\n"}THE R-N PHONT INPUT PACKAGE IS NOT WORKING. CAMERON GOING TO MAKE A PHONE INPUT BLOCK W/ STATE</TextHeader>
-                    <ButtonView>
-                        <SignupButton onPress={() => {
-                            navigation.navigate('SignUpEmail');
-                        }}>
-                            <SignupText>Next</SignupText>
-                        </SignupButton>
-                </ButtonView>
-            </LinearGradient>
-        </Main>
-    )
-}
+        <TextDisclaimer>
+          {'\n'}By entering and tapping Next, you agree to the &nbsp;
+          <Text style={{textDecorationLine: 'underline'}}>Terms,</Text> &nbsp;
+          <Text style={{textDecorationLine: 'underline'}}>E-Sign Consent</Text>
+          &nbsp; & &nbsp;
+          <Text style={{textDecorationLine: 'underline'}}>Privacy Policy</Text>.
+        </TextDisclaimer>
+        <ButtonView>
+          <SignupButton
+            onPress={() => {
+              navigation.navigate('SignUpEmail');
+            }}>
+            <SignupText>Next</SignupText>
+          </SignupButton>
+        </ButtonView>
+      </LinearGradient>
+    </Main>
+  );
+};
 export default SignUpPhone;
